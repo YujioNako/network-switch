bdfca47ba257fc6d7112c8e8ca37f66802c21839
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2024-10-16 15:35:47 +0800

    3.2增加自动检测网络适配器

--- switch.bat
+++ switch.bat
@@ -2,15 +2,47 @@ chcp 65001
 @echo off
 setlocal
 
-echo ---------------------------------------------------------
-echo                         内外网切换脚本 v3.1
+echo -----------------------------------------------
+echo              内外网切换脚本 v3.2
 echo 本脚本由YujioNako编写，使用风险由使用者自行承担
-echo         运行异常则请打开脚本确认并配置必要设置
-echo ---------------------------------------------------------
-REM 请在下方设置WLAN与以太网的名称
-REM 这个两个名称在不同设备上可能不同，请在“控制面板\网络和 Internet\网络连接”处查看
-set WLAN_NAME=WLAN 2
+
+echo         初次使用请打开脚本配置必要设置
+echo -----------------------------------------------
+
+REM 请在下方设置WLAN与以太网的网络适配器名称；这个两个名称在不同设备上可能不同，请在“控制面板\网络和 Internet\网络连接”处查看，也可以通过cmd运行命令 control ncpa.cpl 快捷打开网络连接面板
+set WLAN_NAME=WLAN
 set ETHERNET_NAME=以太网 2
+
+REM 检测网络适配器是否存在
+
+REM 检测WLAN适配器
+netsh interface show interface name="%WLAN_NAME%" >nul
+set wlan=%errorlevel%
+
+REM 检测以太网适配器
+netsh interface show interface name="%ETHERNET_NAME%" >nul
+set ethernet=%errorlevel%
+
+REM 判断两个适配器是否都存在
+if not %wlan%==0 (
+    echo 网络适配器 %WLAN_NAME% 不存在，请打开脚本设置WLAN_NAME
+)
+if not %ethernet%==0 (
+    echo 网络适配器 %ETHERNET_NAME% 不存在，请打开脚本设置ETHERNET_NAME
+) 
+if not %ethernet%==0 (
+    echo 请在弹出的窗口查看以太网的网络适配器名称，之后在脚本内修改ETHERNET_NAME
+    control ncpa.cpl
+    pause
+    exit /b
+) else  if not %wlan%==0 (
+    echo 请在弹出的窗口此查看WLAN的网络适配器名称，之后在脚本内修改WLAN_NAME
+    control ncpa.cpl
+    pause
+    exit /b
+) else (
+    echo 网络适配器已检测
+)
 
 REM 检测WLAN状态
 set WLAN_STATUS=Connected
@@ -24,8 +56,8 @@ for /f "tokens=*" %%i in ('netsh interface show interface name^="%ETHERNET_NAME%
     set ETHERNET_STATUS=Disconnected
 )
 
-echo WLAN_STATUS "%WLAN_STATUS%"
-echo ETHERNET_STATUS "%ETHERNET_STATUS%"
+echo WLAN_STATUS . . . . . . . . %WLAN_STATUS%
+echo ETHERNET_STATUS . . . . . . %ETHERNET_STATUS%
 
 REM 如果WLAN未连接且以太网断开
 if "%WLAN_STATUS%"=="Disconnected" (
@@ -71,8 +103,6 @@ if "%WLAN_STATUS%"=="Disconnected" (
     timeout /t 3
     echo 请自行连接WiFi
     start ms-settings:network-wifi
-    pause
-    exit /b
     pause
     exit /b
   )
